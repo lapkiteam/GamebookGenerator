e76000c431ebe892887b1867ee9f20e1abe47a77
gretmn102 <erlgreywatson@gmail.com>
2026-05-18 05:12:01 +0300

    wip: write ast

---
+++ src/Core/Ast.fsx
@@ -0,0 +1,16 @@
+// module GamebookGenerator.Core.Ast
+
+[<RequireQualifiedAccess>]
+type Content =
+    | Text of string
+    | Link of string
+
+type Paragraph = {
+    Title: string
+    Content: Content
+}
+
+type Document = {
+    Intro: string
+    Paragraphs: Paragraph list
+}
