50a1a714034af648b2d6495f49c69aaac764743c
gretmn102 <erlgreywatson@gmail.com>
2026-05-26 06:17:37 +0300

    feat(printer): write printer for `Inline`

--- src/Twine/SugarCube/Printer/Inline.fs
+++ src/Twine/SugarCube/Printer/Inline.fs
@@ -1,1 +1,14 @@
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+[<RequireQualifiedAccess>]
 module Twine.SugarCube.FSharp.Printer.Inline
+
+open FsharpMyExtension.Serialization.Serializers.ShowList
+
+open Twine.SugarCube.FSharp
+
+let shows (inline': Inline) : ShowS =
+    match inline' with
+    | Inline.Text text ->
+        showString text
+    | Inline.Link link ->
+        Link.shows link
