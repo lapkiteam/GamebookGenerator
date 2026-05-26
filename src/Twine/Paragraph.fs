6b02630cf91fb90265b4afa56e3783173fe299a7
gretmn102 <erlgreywatson@gmail.com>
2026-05-26 06:59:14 +0300

    wip: make `Paragraph`

---
+++ src/Twine/Paragraph.fs
@@ -0,0 +1,22 @@
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+[<RequireQualifiedAccess>]
+module GamebookGenerator.Twine.Paragraph
+open GamebookGenerator.Core.Ast
+open Twine
+
+let toTwine (paragraph: Paragraph) =
+    let header: Twee.FSharp.PassageHeader = {
+        Name = string paragraph.Id
+        Tags = None
+        Metadata = None
+    }
+    let body: Twee.FSharp.PassageBody =
+        [
+            paragraph.Title
+            yield! paragraph.Content |> List.map Line.toTwine
+        ]
+    let passage: Twee.FSharp.Passage = {
+        Header = header
+        Body = body
+    }
+    passage
