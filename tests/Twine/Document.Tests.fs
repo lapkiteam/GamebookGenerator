d36f99b9c94c24a3e317a8343b296b8a8eae6f4f
gretmn102 <erlgreywatson@gmail.com>
2026-05-26 08:38:59 +0300

    test: write test for `Document`

---
+++ tests/Twine/Document.Tests.fs
@@ -0,0 +1,44 @@
+module GamebookGenerator.Twine.Document.Tests
+open Expecto
+open GamebookGenerator.Core.Ast.Helpers
+
+[<Tests>]
+let ``Twine.Document.toTwine`` =
+    testList "Twine.Document.toTwine" [
+        testCase "base" <| fun () ->
+            Expect.equal
+                (toTwine {
+                    Intro = []
+                    Paragraphs = [
+                        {
+                            Id = 1
+                            Title = "Первый параграф"
+                            Content = [
+                                [text "Пойти налево — "; link 1; text ", Пойти направо — "; link 2]
+                            ]
+                        }
+                    ]
+                })
+                [
+                    {
+                        Header = {
+                            Name = "Start"
+                            Tags = None
+                            Metadata = None
+                        }
+                        Body = []
+                    }
+                    {
+                        Header = {
+                            Name = "1"
+                            Tags = None
+                            Metadata = None
+                        }
+                        Body = [
+                            "Первый параграф"
+                            "Пойти налево — [[$1|1]], Пойти направо — [[$2|2]]"
+                        ]
+                    }
+                ]
+                ""
+    ]
