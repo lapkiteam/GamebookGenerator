b87664889f6a864511543cc3173fcb7be16bb101
gretmn102 <erlgreywatson@gmail.com>
2026-05-26 04:53:10 +0300

    wip: add `Ast`

--- src/Twine/SugarCube/Ast.fs
+++ src/Twine/SugarCube/Ast.fs
@@ -1,1 +1,12 @@
 namespace Twine.SugarCube.FSharp
+open Twee.FSharp
+
+type Link = {
+    Text: string
+    PassageName: PassageName
+}
+
+[<RequireQualifiedAccess>]
+type Inline =
+    | Text of string
+    | Link of Link
