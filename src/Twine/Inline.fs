4e162077a36b69ee547b6d4ca52aed89f0be95b6
gretmn102 <erlgreywatson@gmail.com>
2026-05-26 06:50:28 +0300

    wip: make `Inline`

--- src/Twine/Inline.fs
+++ src/Twine/Inline.fs
@@ -2,8 +2,14 @@
 [<RequireQualifiedAccess>]
 module GamebookGenerator.Twine.Inline
 open GamebookGenerator.Core.Ast
+open Twine
 
 let toTwine (inline': Inline) =
     match inline' with
     | Inline.Link paragraphId ->
-        Twee.FSharp.PassageBody.Empty
+        SugarCube.FSharp.Inline.Link {
+            Text = sprintf "$%d" paragraphId
+            PassageName = string paragraphId |> Some
+        }
+    | Inline.Text text ->
+        SugarCube.FSharp.Inline.Text text
