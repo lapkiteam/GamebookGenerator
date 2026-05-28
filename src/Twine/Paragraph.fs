843cff24815a299e07ccfc5f6f09a9c5abdd910d
Agent Lapki <treesortbubble@gmail.com>
2026-05-28 09:44:08 +0300

    chore(dependencies): update `Twine.Twee.FSharp` to `v0.5.0` (#5)

--- src/Twine/Paragraph.fs
+++ src/Twine/Paragraph.fs
@@ -4,17 +4,17 @@ module GamebookGenerator.Twine.Paragraph
 open GamebookGenerator.Core
 
 let toTwine (paragraph: Paragraph) =
-    let header: Twee.FSharp.PassageHeader = {
+    let header: Twine.Twee.FSharp.PassageHeader = {
         Name = string paragraph.Id
         Tags = None
         Metadata = None
     }
-    let body: Twee.FSharp.PassageBody =
+    let body: Twine.Twee.FSharp.PassageBody =
         [
             paragraph.Title
             yield! paragraph.Content |> List.map Line.toTwine
         ]
-    let passage: Twee.FSharp.Passage = {
+    let passage: Twine.Twee.FSharp.Passage<_> = {
         Header = header
         Body = body
     }
